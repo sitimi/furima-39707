a364e3df143e446b724e449ecada059e5c16bd4b
sitimi <sitimi522@gmail.com>
2023-12-30 20:16:01 +0900

    itemsテーブルのfactorybot記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  {Faker::Name.initials(number: 2)}
+    introduction          {Faker::Lorem.sentence}
+    category_id              {'2'}
+    condition_id             {'2'}
+    delivery_charge_id       {'2'}
+    region_id                {'2'}
+    delivery_days_id         {'2'}
+    price                 {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
   end
 end
