e0efc422abf8f8a5c28bc425a8ddc6a9b452b076
sitimi <sitimi522@gmail.com>
2023-12-30 16:27:14 +0900

    itemモデルのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,17 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :region
   belongs_to :until
+
+  validates :image, presence: true, blob: { content_type: :image }
+  validates :name, :introduction, presence: true
+  validates :category_id, :condition_id, :delivery_charge_id, :region_id, :until_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }, presence: true
+
+
 end
