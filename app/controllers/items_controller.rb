6e3dd7bcb36c822d8200a54ca69030ba20858cf1
sitimi <sitimi522@gmail.com>
2023-12-30 17:12:01 +0900

    itemsコントローラーのcreateアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
   def index
     
   end
@@ -7,5 +9,23 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # ストロングパラメーターにはpermit(:image) を追加すること
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image, :name,
+      :introduction, :category_id,
+      :condition_id, :delivery_charge_id,
+      :region_id, :until_id, :price
+    ).merge(user_id: current_user.id)
+  end
+
 end
