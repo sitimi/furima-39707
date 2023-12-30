60b90b86d601fbbf0fb41c2878d2fcb6c362a70d
sitimi <sitimi522@gmail.com>
2023-12-30 15:56:57 +0900

    itemモデルとActive_hashのモデルとのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :region
+  belongs_to :until
 end
