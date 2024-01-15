8caa873651616f2e29a8314d073969c8247c5ceb
sitimi <sitimi522@gmail.com>
2024-01-15 17:27:24 +0900

    エラーが表示されるよう実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
 
