bfe78470285a75a1ba7d83e6d146ff456d6d705a
sitimi <sitimi522@gmail.com>
2024-01-15 17:16:30 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :post_code, :region, :city, :street, :building_name, :phone_number, :order_id
+  attr_accessor :item_id, :user_id, :post_code, :region_id, :city, :street, :building_name, :phone_number, :order_id
 
 
   with_options presence: true do
@@ -8,6 +8,7 @@ class OrderAddress
     validates :user_id
     validates :city
     validates :street
+    validates :region_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
   end
