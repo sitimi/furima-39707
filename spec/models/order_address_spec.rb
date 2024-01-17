3e3eb714fbaa9a06934e78b045d91fd526cf29b2
sitimi <sitimi522@gmail.com>
2024-01-17 17:36:15 +0900

    テストコード修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe OrderAddress, type: :model do
   describe '配送先情報の保存' do
     before do
       user = FactoryBot.create(:user)
-      @order_address = FactoryBot.build(:order_address, user_id: user.id)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id )
     end
 
     context '配送先情報の保存ができるとき' do
@@ -99,6 +100,11 @@ RSpec.describe OrderAddress, type: :model do
       end
       it '電話番号が12桁以上あると保存できないこと' do
         @order_address.phone_number = '1234567890123'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が9桁以下だと保存できないこと' do
+        @order_address.phone_number = '123456789'
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
