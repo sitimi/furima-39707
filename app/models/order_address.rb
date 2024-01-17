2bcf772496ad2b8d0bb9c333ae22e6127df1d14a
sitimi <sitimi522@gmail.com>
2024-01-17 17:04:19 +0900

    電話番号を10桁も許容するよう修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,7 +10,7 @@ class OrderAddress
     validates :street
     validates :region_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
   end
 
   def save
